e0ce54c1f9704c6089b6d6a55e7eb7262e825816
burabure <3amsleep@gmail.com>
2013-01-23 00:30:08 -0300

    Associate Users to Tickets on ticket creation

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,4 +1,5 @@
 class TicketsController < ApplicationController
+  before_filter :authenticate_user!, :except => [:index, :show]
   before_filter :find_project
   before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
 
@@ -8,6 +9,7 @@ class TicketsController < ApplicationController
 
   def create
     @ticket = @project.tickets.build(params[:ticket])
+    @ticket.user = current_user
     if @ticket.save
       redirect_to [@project, @ticket],
                   :notice => "Ticket has been created"
@@ -15,6 +17,9 @@ class TicketsController < ApplicationController
       flash[:alert] = "Ticket has not been created"
       render :action => "new"
     end
+  end
+
+  def index
   end
 
   def show
--- spec/features/viewing_tickets_spec.rb
+++ spec/features/viewing_tickets_spec.rb
@@ -2,17 +2,20 @@ require 'spec_helper'
 
 feature "Viewing tickets" do
   before do
+    user = FactoryGirl.create :user
     textmate_2 = FactoryGirl.build(:project, :name => "TextMate 2")
     FactoryGirl.create(:ticket,
                       :project => textmate_2,
                       :title => "Make it shiny",
-                      :description => "Gradients! Starbursts! Oh my!")
+                      :description => "Gradients! Starbursts! Oh my!",
+                      :user => user)
 
     internet_explorer = FactoryGirl.build(:project)
     FactoryGirl.create(:ticket,
                       :project => internet_explorer,
                       :title => "Standards compliance",
-                      :description => "Isn't a joke.")
+                      :description => "Isn't a joke.",
+                      :user => user)
 
     visit '/'
   end
