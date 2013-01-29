609ae69401328afee1ea8c9bf070fdcfcadcf3a4
burabure <3amsleep@gmail.com>
2013-01-28 23:20:54 -0300

    Fixed Sign Up

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -3,11 +3,14 @@ require "spec_helper"
 feature "Signing up" do
   scenario "Successful sign up" do
     visit '/'
-    click_link "Sign up"
+    within("nav") do
+      click_link "Sign up"
+    end
     fill_in "Email", :with => "user@ticketee.com"
     fill_in "user_password", :with => "password"
     fill_in "user_password_confirmation", :with => "password"
     click_button "Sign up"
     page.should have_content "Please open the link to activate your account"
+    page.should have_content "Please confirm your account before signing in"
   end
 end
--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -3,14 +3,16 @@ require 'spec_helper'
 feature "Viewing Projects" do
   let!(:user) {FactoryGirl.create(:confirmed_user)}
   let!(:project) {FactoryGirl.create(:project)}
+  let!(:project_2) {FactoryGirl.create(:project, :name => "Internet Explorer")}
 
   before do
-    sign_in_as!(user)
     define_permission!(user, :view, project)
+    sign_in_as!(user)
   end
 
   scenario "Listing all Projects" do
     visit '/'
+    page.should_not have_content project_2.name
     click_link project.name
     page.current_url.should == project_url(project)
   end
