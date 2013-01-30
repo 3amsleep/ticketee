f2fd334bbf699cb5e5d2524ebbe910144f4a14eb
burabure <3amsleep@gmail.com>
2013-01-30 15:14:37 -0300

    Restrict actions in TicketsController based on permissions and hide links

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -4,6 +4,7 @@ feature "hidden links" do
   let(:user) {FactoryGirl.create(:confirmed_user)}
   let(:admin) {FactoryGirl.create(:admin_user)}
   let(:project) {FactoryGirl.create(:project)}
+  let!(:ticket) {FactoryGirl.create(:ticket, :project => project, :user => user)}
 
   def link_path(route)
     if route == "project_path"
@@ -39,6 +40,52 @@ feature "hidden links" do
         page.should_not have_link("#{action} Project")
       end
     end
+
+    # Links are shown with permissions
+    scenario "New ticket link is shown to a user with permissions" do
+      define_permission!(user, :view, project)
+      define_permission!(user, :create_tickets, project)
+      visit project_path(project)
+      page.should have_link "New Ticket"
+    end
+
+    scenario "Edit ticket link is shown to a user with permissions" do
+      define_permission!(user, :view, project)
+      define_permission!(user, :edit_tickets, project)
+      visit project_path(project)
+      click_link ticket.title
+      page.should have_link "Edit Ticket"
+    end
+
+    scenario "Delete ticket link is shown to a user with permissions" do
+      define_permission!(user, :view, project)
+      define_permission!(user, :delete_tickets, project)
+      visit project_path(project)
+      click_link ticket.title
+      page.should have_link "Delete Ticket"
+    end
+
+    # Links are hidden without permissions
+    scenario "New ticket link is hidden from a user without permissions" do
+      define_permission!(user, :view, project)
+      visit project_path(project)
+      page.should_not have_link "New Ticket"
+    end
+
+    scenario "Edit ticket link is hidden from a user without permissions" do
+      define_permission!(user, :view, project)
+      visit project_path(project)
+      click_link ticket.title
+      page.should_not have_link "Edit Ticket"
+    end
+
+    scenario "Delete ticket link is shown to a user with permissions" do
+      define_permission!(user, :view, project)
+      visit project_path(project)
+      click_link ticket.title
+      page.should_not have_link "Delete Ticket"
+    end
+
   end
 
   context "Admin Users" do
@@ -51,6 +98,23 @@ feature "hidden links" do
         visit link_path(route)
         page.should have_link("#{action} Project")
       end
+    end
+
+    scenario "New ticket link is shown to admins" do
+      visit project_path(project)
+      page.should have_link "New Ticket"
+    end
+
+    scenario "Edit ticket link is shown to admins" do
+      visit project_path(project)
+      click_link ticket.title
+      page.should have_link "Edit Ticket"
+    end
+
+    scenario "Delete ticket link is shown to admins" do
+      visit project_path(project)
+      click_link ticket.title
+      page.should have_link "Delete Ticket"
     end
   end
 
